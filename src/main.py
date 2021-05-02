4af9083557eac60c82016192e0d23c171b74ebf9
Manuel Montero <manuelmontero@iMac-de-Manuel.local>
2021-05-02 11:37:28 +0200

    commit

---
+++ src/main.py
@@ -0,0 +1,68 @@
+from note_seq.protobuf import music_pb2
+from google.colab import files
+import magenta
+import note_seq
+import tensorflow
+import os
+os.chdir("/Users/manuelmontero/PycharmProjects/NEXT")
+
+# Here's another NoteSequence!
+teapot = music_pb2.NoteSequence()
+teapot.notes.add(pitch=69, start_time=0, end_time=0.5, velocity=80)
+teapot.notes.add(pitch=71, start_time=0.5, end_time=1, velocity=80)
+teapot.notes.add(pitch=73, start_time=1, end_time=1.5, velocity=80)
+teapot.notes.add(pitch=74, start_time=1.5, end_time=2, velocity=80)
+teapot.notes.add(pitch=76, start_time=2, end_time=2.5, velocity=80)
+teapot.notes.add(pitch=81, start_time=3, end_time=4, velocity=80)
+teapot.notes.add(pitch=78, start_time=4, end_time=5, velocity=80)
+teapot.notes.add(pitch=81, start_time=5, end_time=6, velocity=80)
+teapot.notes.add(pitch=76, start_time=6, end_time=8, velocity=80)
+teapot.total_time = 8
+
+teapot.tempos.add(qpm=60);
+
+note_seq.plot_sequence(teapot)
+note_seq.sequence_proto_to_midi_file(teapot, 'resource/generated/output/example.mid')
+
+
+
+
+
+
+print('Downloading model bundle. This will take less than a minute...')
+note_seq.notebook_utils.download_bundle('basic_rnn.mag', 'content/')
+
+# Import dependencies.
+from magenta.models.melody_rnn import melody_rnn_sequence_generator
+from magenta.models.shared import sequence_generator_bundle
+from note_seq.protobuf import generator_pb2
+from note_seq.protobuf import music_pb2
+
+# Initialize the model.
+print("Initializing Melody RNN...")
+bundle = sequence_generator_bundle.read_bundle_file('content/basic_rnn.mag')
+generator_map = melody_rnn_sequence_generator.get_generator_map()
+melody_rnn = generator_map['basic_rnn'](checkpoint=None, bundle=bundle)
+melody_rnn.initialize()
+
+# Model options. Change these to get different generated sequences!
+input_sequence = teapot # change this to teapot if you want
+num_steps = 128 # change this for shorter or longer sequences
+temperature = 1.0 # the higher the temperature the more random the sequence.
+
+# Set the start time to begin on the next step after the last note ends.
+last_end_time = (max(n.end_time for n in input_sequence.notes)
+                  if input_sequence.notes else 0)
+qpm = input_sequence.tempos[0].qpm
+seconds_per_step = 60.0 / qpm / melody_rnn.steps_per_quarter
+total_seconds = num_steps * seconds_per_step
+
+generator_options = generator_pb2.GeneratorOptions()
+generator_options.args['temperature'].float_value = temperature
+generate_section = generator_options.generate_sections.add(
+  start_time=last_end_time + seconds_per_step,
+  end_time=total_seconds)
+
+# Ask the model to continue the sequence.
+sequence = melody_rnn.generate(input_sequence, generator_options)
+note_seq.sequence_proto_to_midi_file(teapot, 'resource/generated/output/example.mid')
